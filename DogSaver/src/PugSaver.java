3725f577cfd995c70665fb450cd53c19fc805c7e
pad0may <dmartin1@hwemail.com>
2025-08-28 01:43:42 -0700

    thursday 28 try 2

--- DogSaver/src/PugSaver.java
+++ DogSaver/src/PugSaver.java
@@ -2,13 +2,9 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class PugSaver {
+
 	//Moves every dog whose breed is "Pug" in the list to the back of the list
 	public static void rescuePugs(ArrayList<Dog> list) {
-    for (int i = list.size(); i < max; i++) {
-      if (list.get(i).getBreed()) {
-        
-      }
-     }
-    }
+
 	}
 }
