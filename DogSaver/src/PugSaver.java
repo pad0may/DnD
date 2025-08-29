2c2e7e70be542a895b28cfc0009329b819b10d3f
pad0may <dmartin1@hwemail.com>
2025-08-29 11:17:10 -0700

    Friday 08/29

--- DogSaver/src/PugSaver.java
+++ DogSaver/src/PugSaver.java
@@ -3,8 +3,25 @@ import java.util.Objects;
 
 public class PugSaver {
 
-	//Moves every dog whose breed is "Pug" in the list to the back of the list
-	public static void rescuePugs(ArrayList<Dog> list) {
+  private static int dogCount = 0;
+  private static Dog tempDog;
 
-	}
+  // Moves every dog whose breed is "Pug" in the list to the back of the list
+  public static void rescuePugs(ArrayList<Dog> list) {
+    for (int i = list.size(); i < list.size(); i++) {
+      if (list.get(i).getBreed().substring(0, 5).equals("golden")) {
+        for (int x = i; x < list.size() - i; x++) {
+          if (!list.get(x).getBreed().substring(0, 5).equals("golden")) {
+            tempDog = list.get(x);
+            list.set(x, list.get(i));
+            list.set(i, tempDog);
+          }
+        }
+      }
+      dogCount++;
+      if (dogCount == list.size()) {
+        break;
+      }
+    }
+  }
 }
---
+++ DogSaver/src/test.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class test {
+  public static void main(String[] args) {
+    ArrayList list = new ArrayList<>();
+    System.out.println(list.size());
+    System.out.println("test");
+  }
+}
