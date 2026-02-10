d434d44219ca4478e957cf3cfe35dad2383df333
Dylan Martin <143762725+pad0may@users.noreply.github.com>
2026-02-10 09:18:03 -0800

    2/10/26

---
+++ HeapPQ/HeapPQ.java
@@ -0,0 +1,117 @@
+
+public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
+
+	private E[] heap;
+	private int objectCount;
+
+	public HeapPQ() {
+		this.heap = (E[])new Comparable[3];
+		this.objectCount = 0;
+	}
+
+	//Returns the number of elements in the priority queue
+	public int size() {
+		return objectCount;
+	}
+
+	//DO NOT CHANGE MY JANKY TOSTRING!!!!!
+	public String toString() {
+		StringBuffer stringbuf = new StringBuffer("[");
+		for (int i = 0; i < objectCount; i++) {
+			stringbuf.append(heap[i]);
+			if (i < objectCount - 1)
+				stringbuf.append(", ");
+		}
+		stringbuf.append("]\nor alternatively,\n");
+
+		for(int rowLength = 1, j = 0; j < objectCount; rowLength *= 2) {
+			for (int i = 0; i < rowLength && j < objectCount; i++, j++) {
+				stringbuf.append(heap[j] + " ");
+			}
+			stringbuf.append("\n");
+			if (j < objectCount) {
+				for (int i = 0; i < Math.min(objectCount - j, rowLength*2); i++) {
+					if (i%2 == 0)
+						stringbuf.append("/");
+					else
+						stringbuf.append("\\ ");
+				}
+				stringbuf.append("\n");
+			}
+		}
+		return stringbuf.toString();
+	}
+
+	//Doubles the size of the heap array
+	private void increaseCapacity() {
+		E[] newHeap = (E[])new Comparable[heap.length*2];
+		for (int i = 0; i < heap.length; i++) {
+			newHeap[i] = heap[i];
+		}
+		this.heap = newHeap;
+	}
+
+	//Returns the index of the "parent" of index i
+	private int parent(int i) {
+		return (i - 1) / 2;
+	}
+	//Returns the index of the *smaller child* of index i
+	private int smallerChild(int i) {
+		return i * 2 + 1;
+	}
+
+	//Swaps the contents of indices i and j
+	private void swap(int i, int j) {
+		if (i < 0 || j < 0) return;
+		E temp = heap[j];
+		heap[j] = heap[i];
+		heap[i] = temp;
+	}
+
+	// Bubbles the element at index i upwards until the heap properties hold again.
+	private void bubbleUp(int i) {
+		int j = parent(i);
+		while (heap[i].compareTo(heap[j]) > 0) {
+			swap(i, j);
+			i = j;
+			j = parent(i);
+		}
+	}
+
+	// Bubbles the element at index i downwards until the heap properties hold again.
+	private void bubbleDown(int i) {
+		int j = smallerChild(i);
+		while (heap[i].compareTo(heap[j]) < 0) {
+			swap(i, j);
+			i = j;
+			j = smallerChild(i);
+		}
+	}
+
+	@Override
+	public void add(E obj) {
+		if (objectCount == heap.length) increaseCapacity();
+		heap[objectCount] = obj;
+		bubbleUp(objectCount);
+		objectCount++;
+	}
+
+	@Override
+	public E removeMin() {
+		E ret = heap[objectCount-1];
+		objectCount--;
+		heap[objectCount-1] = null;
+		return ret;
+	}
+
+	@Override
+	public E peek() {
+		return heap[0];
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return objectCount == 0;
+	}
+
+}
